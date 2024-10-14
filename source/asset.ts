1b82b67ae78eb180e6c73797c3abcceb8b52236f
TechQuery <shiy2008@gmail.com>
2024-10-15 01:38:58 +0800

    [fix] find Current Module Path in Anonymous Scope [migrate] upgrade to Marked 14, Node.js 20, PNPM 9, Husky 9 & other latest Upstream packages/actions [optimize] support NPM provenance

--- source/asset.ts
+++ source/asset.ts
@@ -1,15 +1,17 @@
-import { marked } from 'marked';
+import { Marked } from 'marked';
+import { markedHighlight } from 'marked-highlight';
 import { highlight, languages } from 'prismjs';
 import loadLanguages from 'prismjs/components/';
 
-marked.setOptions({
-    highlight(code, language) {
-        loadLanguages([language]);
+export const marked = new Marked(
+    markedHighlight({
+        highlight(code, language) {
+            loadLanguages([language]);
 
-        return highlight(code, languages[language], language);
-    }
-});
-
+            return highlight(code, languages[language], language);
+        }
+    })
+);
 export function makeMarkdownTable(data: Record<string, string>[]) {
     const keys = Object.keys(data[0]);
 
