277dd5fb5501d82f752ee1bb2c7cf9493787c79d
South Drifter <shiy2008@gmail.com>
2025-11-01 03:12:06 +0800

    [remove] outdated Console, Network, Language & Asset functions (#7)

--- source/language.ts
+++ source/language.ts
@@ -1,91 +1,10 @@
-import { TransformOptions, transform } from '@babel/core';
-import { format } from 'prettier';
-import { minify } from 'uglify-js';
 import { deepStrictEqual } from 'assert';
-
-/**
- * generate a Number with 36 radix
- */
-export function uniqueID() {
-    return Number((Math.random() + '').slice(2)).toString(36);
-}
 
 export function toRegExp(raw: string) {
     const [source, flag] =
         raw.match(/^\/?(.+?)(?:\/([a-z]+)?)?$/)?.slice(1) || [];
 
     if (source) return new RegExp(source, flag);
-}
-
-export function prettify(source: string) {
-    return format(source, {
-        parser: 'babel',
-        singleQuote: true,
-        trailingComma: 'none',
-        arrowParens: 'avoid',
-        tabWidth: 4
-    }).trim();
-}
-
-/**
- * @param code       ES 6+ source code
- * @param fileName   Full name of this JS file
- * @param onlyModule Only transform ES 6 module to CommonJS
- *
- * @return ES 5 source code
- */
-export function toES_5(code: string, fileName?: string, onlyModule?: boolean) {
-    const option: TransformOptions = {
-        ...(onlyModule ? null : { presets: ['@babel/preset-env'] }),
-        plugins: ['@babel/plugin-transform-modules-commonjs'],
-        ast: false,
-        filename: fileName || undefined,
-        babelrc: !onlyModule,
-        parserOpts: {
-            plugins: [
-                'optionalCatchBinding',
-                'objectRestSpread',
-                'asyncGenerators',
-                'classProperties',
-                ['decorators', { decoratorsBeforeExport: true }],
-                'importMeta',
-                'dynamicImport',
-                'exportDefaultFrom',
-                'exportNamespaceFrom'
-            ]
-        }
-    };
-    return prettify(
-        transform(code, option).code.replace(
-            /^(?:'|")use strict(?:'|");\n+/,
-            ''
-        )
-    );
-}
-
-export function uglify(source: string, fileName: string) {
-    const { error, code, map } = minify(
-        {
-            [fileName]: source
-        },
-        {
-            mangle: {
-                keep_fnames: true
-            },
-            output: {
-                comments: 'some'
-            },
-            ie8: true,
-            sourceMap: {
-                filename: fileName,
-                url: `${fileName}.map`
-            },
-            warnings: 'verbose'
-        }
-    );
-    if (error) throw error;
-
-    return { code, map };
 }
 
 /**
@@ -110,8 +29,8 @@ export function cache<T extends (...data: any[]) => any>(func: T) {
     } as T;
 }
 
-export function hasItem<T>(list: ArrayLike<T>, value: T) {
-    return Array.from(list).some(item => {
+export const hasItem = <T>(list: ArrayLike<T>, value: T) =>
+    Array.from(list).some(item => {
         try {
             deepStrictEqual(item, value);
             return true;
@@ -119,8 +38,6 @@ export function hasItem<T>(list: ArrayLike<T>, value: T) {
             return false;
         }
     });
-}
-
 export type Patchable<T> = Record<string, T> | ArrayLike<T>;
 
 /**
--- test/asset.spec.ts
+++ test/asset.spec.ts
@@ -1,20 +1,6 @@
-import { makeMarkdownTable, marked } from '../source/asset';
+import { makeMarkdownTable } from '../source/asset';
 
 describe('Asset files', () => {
-    it('should parse Markdown files with Code Highlight', () => {
-        const code = marked.parse(
-            `
-\`\`\`html
-<html></html>
-\`\`\`
-        `
-        );
-        expect(code).toBe(
-            `<pre><code class="language-html"><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>html</span><span class="token punctuation">></span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>html</span><span class="token punctuation">></span></span>
-</code></pre>`
-        );
-    });
-
     it('should render an Object Array to a Markdown table', () => {
         const code = makeMarkdownTable([
             { June: '6', forth: '4' },
